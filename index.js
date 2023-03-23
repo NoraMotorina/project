de0367ed7a5d759c3821bf7d5767751bbf67bed9
Eleonora <dubininaeleonora1204@gmail.com>
2023-03-23 11:19:32 +0200

    new fail 23.03.23

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const number0fFilms = +prompt("How many filmes did you watch?", "");
+
+const personalMovieDB = {
+  count: number0fFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("What did you watch", ""),
+  b = prompt("What stars", ""),
+  c = prompt("What did you watch", ""),
+  d = prompt("What stars", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
